5975710388518bc5151b3997bec5f36a53ab0ae3
ivaninbox <ix.pukhov@gmail.com>
2024-03-12 21:42:01 +0500

    client full

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Header from "./Components/Header/Header";
+import Auth from './Components/Auth/Auth';
+import Main from './Components/Main/Main';
+import Login from './Components/Main/Login';
+import Register from './Components/Main/Register';
+import './App.scss';
+import {useAuth} from "./AuthContext";
+import Catalog from "./Components/Catalog/Catalog";
+import Profile from "./Components/Profile";
+import BottomBar from "./Components/Bottombar";
+import AllCategories from "./Components/Main/AllCategories";
+import Category from "./Components/Main/Category";
+import CategoryReview from "./Components/Main/CategoryReview";
+import ProtectedRoute from './Components/ProtectedRoute';
+import AllReviews from "./Components/Main/AllReviews";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {isAuthenticated} = useAuth();
+
+    return (<div className="App">
+        <Router>
+            <Header/>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/category/" element={<AllCategories/>}/>
+                    <Route path="/catalog" element={<Catalog/>}/>
+                    <Route path="/auth" element={<Auth/>}/>
+                    <Route path="/reviews" element={<AllReviews/>}/>
+                    <Route path="/review/category/:categoryId/" element={<CategoryReview/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route element={<ProtectedRoute/>}> {/* Используйте ProtectedRoute */}
+                        <Route path="/profile" element={<Profile/>}/>
+                    </Route>
+                    <Route path="/category/:categoryId" element={<Category/>}/>
+                </Routes>
+            </div>
+            <BottomBar/>
+        </Router>
+    </div>);
 }
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import profile from '../../img/account.png'
+import menu from '../../img/free-icon-burger-bar-6065135.png'
+import React from "react";
+
+const Header = () => {
+  return(
+      <header className="header">
+          <div className="header__logo">
+              <h2 className="logo__title">Reviewer</h2>
+          </div>
+
+      </header>
+  )
+}
+
+export default Header
